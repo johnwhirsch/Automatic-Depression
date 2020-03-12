f9926886ca90bbe2ede99c2635290ba2620afe86
DRENREPUS <johnwhirsch@gmail.com>
2020-03-12 12:29:37 -0400

    Update stock-updates.ps1

--- stock-updates.ps1
+++ stock-updates.ps1
@@ -1,16 +1,30 @@
-# This script will run a curl request to get the current index prices and read them out loud to you every 15 minutes
-
 Add-Type -AssemblyName System.speech
 $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
 
-do{
-    $indexes = $(curl -uri https://financialmodelingprep.com/api/v3/majors-indexes).content | ConvertFrom-Json
-    foreach($index in $indexes.majorIndexesList){
-        if($index.ticker -like ".*"){
-            if($index.changes -like "-*"){ $changeVerb = "down" }else{ $changeVerb = "up" }
+do{        
+    $webrequest = (Invoke-WebRequest -Uri https://money.cnn.com/data/markets/dow/)
+    $table = $webrequest.ParsedHtml.getElementById('wsod_quoteLeft')
 
-            $speak.Speak("The $($index.indexName) is currently at $($index.price), which is $($changeVerb) $($index.changes) for the hour.")
-        }
+    foreach($row in $($table.getElementsByTagName('tr'))){
+
+        $cells = $row.getElementsByTagName('td')
+        $innerTextFound = $false;
+
+        foreach($cell in $cells){ if($cell.innerText -eq "Open"){ $innerTextFound = $true; } }
+        if($innerTextFound){ $openPrice = [int]$($cells[1].innerText) }
     }
+
+
+    $changeCell = $webrequest.ParsedHtml.getElementsByTagName('td') | ? { $_.getAttributeNode('class').value -eq "wsod_change" }
+    $dailyChange = $($changeCell.children)[1].innerText
+
+    $currentCell = $webrequest.ParsedHtml.getElementsByTagName('td') | ? { $_.getAttributeNode('class').value -eq "wsod_last wsod_lastIndex" }
+    $currentPrice = $($currentCell.children)[0].innerText
+
+    if($dailyChange -like "-*"){ $changeVerb = "down" }else{ $changeVerb = "up" }
+    
+    $speak.Speak("The DOW is currently at $($currentPrice), which is $($changeVerb) $($dailyChange) for the day.")
+       
     sleep -Seconds 900
+
 }while( 1 -eq 1 )
